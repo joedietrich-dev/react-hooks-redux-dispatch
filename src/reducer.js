2e0de47d453f0648791ea1f8946abecaa54eafc4
joedietrich-dev <joe@joedietrich.net>
2022-01-04 21:29:08 -0500

    commit tree 4b411e8cc3b1a6e90fec1bea09e1310a79e252b8

--- src/reducer.js
+++ src/reducer.js
@@ -2,6 +2,8 @@ function changeState(state, action) {
   switch (action.type) {
     case "counter/increment":
       return { count: state.count + 1 };
+    case "counter/decrement":
+      return { count: state.count - 1 };
     default:
       return state;
   }
@@ -9,5 +11,10 @@ function changeState(state, action) {
 
 let state = { count: 0 };
 let action = { type: "counter/increment" };
+
+function dispatch(action) {
+  state = changeState(state, action);
+  return state;
+}
 
 changeState(state, action);
