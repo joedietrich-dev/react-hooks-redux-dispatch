9ffbaebbba83cdc407444431a2e9ebe712b65060
joedietrich-dev <joe@joedietrich.net>
2022-01-04 21:48:18 -0500

    Done.

--- src/reducer.js
+++ src/reducer.js
@@ -2,6 +2,8 @@ function changeState(state, action) {
   switch (action.type) {
     case "counter/increment":
       return { count: state.count + 1 };
+    case "counter/decrement":
+      return { count: state.count - 1 };
     default:
       return state;
   }
@@ -10,4 +12,19 @@ function changeState(state, action) {
 let state = { count: 0 };
 let action = { type: "counter/increment" };
 
-changeState(state, action);
+function render() {
+  document.querySelector("#count").innerHTML = state.count;
+}
+function dispatch(action) {
+  state = changeState(state, action);
+  render();
+  return state;
+}
+
+const upButt = document.querySelector("#up");
+const downButt = document.querySelector("#down");
+
+upButt.addEventListener("click", () => dispatch({ type: "counter/increment" }));
+downButt.addEventListener("click", () => dispatch({ type: "counter/decrement" }));
+
+render();
